9f338f8aa239d55d425d4ae81830affefb11ec60
gkehne <greg.kehne@gmail.com>
2016-08-10 21:37:12 -0400

    tidying up code

--- displaytiling.py
+++ displaytiling.py
@@ -1,121 +1,127 @@
-from PIL import Image, ImageDraw, ImageOps
+# Gregory Kehne
+#
+# This contains the code to display block tilings in one of a few fomrats.
 
-#choose which one to use: printbrd is text ouptut
-#picturebrd is image output of two types: temporary .bmp and permanent .jpg
-#(choose between them by selectively commenting out lines at the bottom of this)
+from PIL import Image, ImageDraw
+
+
+# this displays the boards in the desired format.
+# "text" gives text output.
+# "temp" gives temporary image output.
+# "filename" gives "filename.jpg" output.
 def disp(board, displaytype):
-	dt=displaytype.lower()
-	if dt=="text":
+	dt = displaytype.lower()
+	if dt == "text":
 		textbrd(board)
-	elif dt=="temp":
+	elif dt == "temp":
 		picturebrd(board, displaytype, False)
 	else:
 		picturebrd(board, displaytype, True)
 	return
 
-#text board output
-def textbrd(b):
-	output=""
-	for row in b:
-		r1,r2,r3=[],[],[]
-		for ele in row:
-			if ele==1:
-				r1=r1+["+","1","+"]
-				r2=r2+["+","1","+"]
-				r3=r3+["+","+","+"]
-			if ele==2:
-				r1=r1+["+","+","+"]
-				r2=r2+["2","2","+"]
-				r3=r3+["+","+","+"]
-			if ele==3:
-				r1=r1+["+","+","+"]
-				r2=r2+["+","3","+"]
-				r3=r3+["+","3","+"]
-			if ele==4:
-				r1=r1+["+","+","+"]
-				r2=r2+["+","4","4"]
-				r3=r3+["+","+","+"]
-			if ele==5:
-				r1=r1+["+","5","+"]
-				r2=r2+["+","5","+"]
-				r3=r3+["+","5","+"]
-			if ele==6:
-				r1=r1+["+","+","+"]
-				r2=r2+["6","6","6"]
-				r3=r3+["+","+","+"]
-			if ele==0:
-				r1=r1+["+","+","+"]
-				r2=r2+["+","0","+"]
-				r3=r3+["+","+","+"]
 
-		r,s,t="","",""
+# text board output
+def textbrd(b):
+	output = ""
+	for row in b:
+		r1, r2, r3 = [], [], []
+		for ele in row:
+			if ele == 1:
+				r1 = r1 + ["+", "1", "+"]
+				r2 = r2 + ["+", "1", "+"]
+				r3 = r3 + ["+", "+", "+"]
+			if ele == 2:
+				r1 = r1 + ["+", "+", "+"]
+				r2 = r2 + ["2", "2", "+"]
+				r3 = r3 + ["+", "+", "+"]
+			if ele == 3:
+				r1 = r1 + ["+", "+", "+"]
+				r2 = r2 + ["+", "3", "+"]
+				r3 = r3 + ["+", "3", "+"]
+			if ele == 4:
+				r1 = r1 + ["+", "+", "+"]
+				r2 = r2 + ["+", "4", "4"]
+				r3 = r3 + ["+", "+", "+"]
+			if ele == 5:
+				r1 = r1 + ["+", "5", "+"]
+				r2 = r2 + ["+", "5", "+"]
+				r3 = r3 + ["+", "5", "+"]
+			if ele == 6:
+				r1 = r1 + ["+", "+", "+"]
+				r2 = r2 + ["6", "6", "6"]
+				r3 = r3 + ["+", "+", "+"]
+			if ele == 0:
+				r1 = r1 + ["+", "+", "+"]
+				r2 = r2 + ["+", "0", "+"]
+				r3 = r3 + ["+", "+", "+"]
+
+		r, s, t = "", "", ""
 		for n in range(len(r1)):
-			r=r+r1[n]+" "
-			s=s+r2[n]+" "
-			t=t+r3[n]+" "
-		output+= r+'\n'+s+'\n'+t+'\n'
+			r = r + r1[n] + " "
+			s = s + r2[n] + " "
+			t = t + r3[n] + " "
+		output += r + '\n' + s + '\n' + t + '\n'
 	print output
 
-#image board output
+
+# image board output
 def picturebrd(b, name, saveit):
-	if len(b)==0:
+	if len(b) == 0:
 		print "ERROR: Tried to display an empty board!"
 		return
 
-	b=zip(*b) #transpose b so the picture works
+	b = zip(*b)  # transpose b so the picture works
 
-	height=32*len(b)
-	width=32*len(b)
+	height = 32 * len(b)
+	width = 32 * len(b)
 
-	#new image created
-	im = Image.new('RGBA', (height, width), (255, 255, 255, 0)) 
-	draw = ImageDraw.Draw(im) 
+	# new image created
+	im = Image.new('RGBA', (height, width), (255, 255, 255, 0))
+	draw = ImageDraw.Draw(im)
 
-	color=(0,0,0,0) #bline color
-	w=1 #width for line
+	color = (0, 0, 0, 0)  # bline color
+	w = 1  # width for line
 
 	for i in range(len(b)):
-		xZ=32*i
+		xZ = 32 * i
 		for j in range(len(b[i])):
-			yZ=32*j
-			#standard interior of cell points
-			p1=(xZ+4,yZ+4)
-			p2=(xZ+4,yZ+28)
-			p3=(xZ+28,yZ+28)
-			p4=(xZ+28,yZ+4)
-			#display cases
-			if b[i][j]==0:
-				draw.line([p1,p2], fill=color, width=w)
-				draw.line([p2,p3], fill=color, width=w)
-				draw.line([p3,p4], fill=color, width=w)
-				draw.line([p4,p1], fill=color, width=w)
-			if b[i][j]==1:
-				draw.line([(xZ+4,yZ),p2], fill=color, width=w)
-				draw.line([p2,p3], fill=color, width=w)
-				draw.line([p3,(xZ+28, yZ)], fill=color, width=w)
-			if b[i][j]==2:
-				draw.line([(xZ,yZ+28),p3], fill=color, width=w)
-				draw.line([p3,p4], fill=color, width=w)
-				draw.line([p4,(xZ,yZ+4)], fill=color, width=w)
-			if b[i][j]==3:
-				draw.line([(xZ+4,yZ+32),p1], fill=color, width=w)
-				draw.line([p4,p1], fill=color, width=w)
-				draw.line([p4,(xZ+28, yZ+32)], fill=color, width=w)
-			if b[i][j]==4:
-				draw.line([(xZ+32,yZ+4),p1], fill=color, width=w)
-				draw.line([p1,p2], fill=color, width=w)
-				draw.line([p2,(xZ+32, yZ+28)], fill=color, width=w)
-			if b[i][j]==5: #vertical?
-				draw.line([(xZ,yZ+4),(xZ+32,yZ+4)], fill=color, width=w)
-				draw.line([(xZ,yZ+28),(xZ+32,yZ+28)], fill=color, width=w)
-			if b[i][j]==6: #horizontal?
-				draw.line([(xZ+4,yZ),(xZ+4,yZ+32)], fill=color, width=w)
-				draw.line([(xZ+28,yZ),(xZ+28,yZ+32)], fill=color, width=w)
-	#output choice:
+			yZ = 32 * j
+			# standard interior of cell points
+			p1 = (xZ + 4, yZ + 4)
+			p2 = (xZ + 4, yZ + 28)
+			p3 = (xZ + 28, yZ + 28)
+			p4 = (xZ + 28, yZ + 4)
+			# display cases
+			if b[i][j] == 0:
+				draw.line([p1, p2], fill=color, width=w)
+				draw.line([p2, p3], fill=color, width=w)
+				draw.line([p3, p4], fill=color, width=w)
+				draw.line([p4, p1], fill=color, width=w)
+			if b[i][j] == 1:
+				draw.line([(xZ + 4, yZ), p2], fill=color, width=w)
+				draw.line([p2, p3], fill=color, width=w)
+				draw.line([p3, (xZ + 28, yZ)], fill=color, width=w)
+			if b[i][j] == 2:
+				draw.line([(xZ, yZ + 28), p3], fill=color, width=w)
+				draw.line([p3, p4], fill=color, width=w)
+				draw.line([p4, (xZ, yZ + 4)], fill=color, width=w)
+			if b[i][j] == 3:
+				draw.line([(xZ + 4, yZ + 32), p1], fill=color, width=w)
+				draw.line([p4, p1], fill=color, width=w)
+				draw.line([p4, (xZ + 28, yZ + 32)], fill=color, width=w)
+			if b[i][j] == 4:
+				draw.line([(xZ + 32, yZ + 4), p1], fill=color, width=w)
+				draw.line([p1, p2], fill=color, width=w)
+				draw.line([p2, (xZ + 32, yZ + 28)], fill=color, width=w)
+			if b[i][j] == 5:  # vertical?
+				draw.line([(xZ, yZ + 4), (xZ + 32, yZ + 4)], fill=color, width=w)
+				draw.line([(xZ, yZ + 28), (xZ + 32, yZ + 28)], fill=color, width=w)
+			if b[i][j] == 6:  # horizontal?
+				draw.line([(xZ + 4, yZ), (xZ + 4, yZ + 32)], fill=color, width=w)
+				draw.line([(xZ + 28, yZ), (xZ + 28, yZ + 32)], fill=color, width=w)
+	# output choice:
 	if not saveit:
 		im.show()
 	else:
-		im.save(name+".jpg")
+		im.save(name + ".jpg")
 	return
-
-
--- tiler.py
+++ tiler.py
@@ -1,195 +1,215 @@
-#Gregory Kehne
+# Gregory Kehne
 #
-#(about)
+# (about)
 
 import displaytiling
 import random
 
-#length of tile. This works for tile=2, tile>=3 is increasingly unreliable. 
-tilesize=2
+# length of tile. This works for tile=2, tile>=3 is increasingly unreliable.
+tilesize = 2
 
-#initializes an empty bxb matrix
+
+# initializes an empty bxb matrix
 def initboard(b):
-	board=[]
+	board = []
 	for i in range(b):
 		board.append([])
 		for j in range(b):
 			board[i].append(0)
 	return board
 
-#this adds a tile to board at spot r,c in direction s
-def fill(board,r,c,s):
-	b=len(board)
-	#x,y,z are beginning, middle, and end squares for a tile
-	if s=="u": x,y,z=1,6,3
-	if s=="d": x,y,z=3,6,1
-	if s=="r": x,y,z=4,5,2
-	if s=="l": x,y,z=2,5,4
-	u,v=0,0
+
+# this adds a tile to board at spot r, c in direction s
+def fill(board, r, c, s):
+	b = len(board)
+	# x, y, z are beginning, middle, and end squares for a tile
+	if s == "u":
+		x, y, z = 1, 6, 3
+	if s == "d":
+		x, y, z = 3, 6, 1
+	if s == "r":
+		x, y, z = 4, 5, 2
+	if s == "l":
+		x, y, z = 2, 5, 4
+	u, v = 0, 0
 
 	for i in range(tilesize):
-		if 0<=r+u and r+u<b and 0<=c+v and c+v<b:
-			if i==0:
-				board[r+u][c+v]=x
-			if i==tilesize-1:
-				board[r+u][c+v]=z
-			if i>0 and i<tilesize-1:
-				board[r+u][c+v]=y
-		#update the relevant direction for the loop
-		if s=="d": u=u+1
-		if s=="u": u=u-1
-		if s=="l": v=v-1
-		if s=="r": v=v+1
+		if 0 <= r + u and r + u < b and 0 <= c + v and c + v < b:
+			if i == 0:
+				board[r + u][c + v] = x
+			if i == tilesize - 1:
+				board[r + u][c + v] = z
+			if i > 0 and i < tilesize - 1:
+				board[r + u][c + v] = y
+		# update the relevant direction for the loop
+		if s == "d":
+			u = u + 1
+		if s == "u":
+			u = u - 1
+		if s == "l":
+			v = v - 1
+		if s == "r":
+			v = v + 1
 
-#checks to see if a tile can be placed on board at r,c in direction s
-def checkdir(board,r,c,s):
-	b=len(board)
-	x,y=0,0
-	boo=True
+
+# checks to see if a tile can be placed on board at r, c in direction s
+def checkdir(board, r, c, s):
+	b = len(board)
+	x, y = 0, 0
+	boo = True
 	for i in range(tilesize):
-		if 0<=r+x and r+x<b and 0<=c+y and c+y<b: #inside the box
-			if board[r+x][c+y]!=0:
-				boo=False
-		#update the relevant direction
-		if s=="d":
-			x=x+1
-		if s=="u": 
-			x=x-1
-		if s=="l": 
-			y=y-1
-		if s=="r": 
-			y=y+1
+		# if its inside the box
+		if 0 <= r + x and r + x < b and 0 <= c + y and c + y < b:
+			if board[r + x][c + y] != 0:
+				boo = False
+		# update the relevant direction
+		if s == "d":
+			x = x + 1
+		if s == "u":
+			x = x - 1
+		if s == "l":
+			y = y - 1
+		if s == "r":
+			y = y + 1
 	return boo
 
-#Check to see if the board has 'contradictions' (unfillable spaces) in it or not.
-#Possible problems for tile>2
+
+# Check to see if the board has 'contradictions' (unfillable spaces) in it
+# or not. Possible problems for tile>2
 def check(board):
-	b=len(board)
+	b = len(board)
 	for r in range(b):
 		for c in range(b):
-			if board[r][c]==0 and not(checkdir(board,r,c,"u") or checkdir(board,r,c,"d") or checkdir(board,r,c,"l") or checkdir(board,r,c,"r")):
+			if board[r][c] == 0 and not (checkdir(board, r, c, "u") or
+											checkdir(board, r, c, "d") or
+											checkdir(board, r, c, "l") or
+											checkdir(board, r, c, "r")):
 				return False
 	return True
 
-#Creates a list of potential next moves to choose randomly from.
+
+# Creates a list of potential next moves to choose randomly from.
 def findSpots(board):
-	b=len(board)
-	#array of potential next moves
-	spots=[]
-	marker=False
-	#scan diagonal for next moves
-	for rc in range(2*b):
+	b = len(board)
+	# array of potential next moves
+	spots = []
+	marker = False
+	# scan diagonal for next moves
+	for rc in range(2 * b):
 		if marker:
 			continue
 		for i in range(b):
-			if rc-i<b and board[i][rc-i]==0 and neighbors(board,i,rc-i)==2:
-				if checkdir(board,i,rc-i,"u"):
-					spots.append([i,rc-i,"u"])
+			if (rc - i < b and
+				board[i][rc - i] == 0 and
+				neighbors(board, i, rc - i) == 2):
+				if checkdir(board, i, rc - i, "u"):
+					spots.append([i, rc - i, "u"])
 					if not marker:
-						marker=True
-				if checkdir(board,i,rc-i,"d"):
-					spots.append([i,rc-i,"d"])
+						marker = True
+				if checkdir(board, i, rc - i, "d"):
+					spots.append([i, rc - i, "d"])
 					if not marker:
-						marker=True
-				if checkdir(board,i,rc-i,"l"):
-					spots.append([i,rc-i,"l"])
+						marker = True
+				if checkdir(board, i, rc - i, "l"):
+					spots.append([i, rc - i, "l"])
 					if not marker:
-						marker=True
-				if checkdir(board,i,rc-i,"r"):
-					spots.append([i,rc-i,"r"])
+						marker = True
+				if checkdir(board, i, rc - i, "r"):
+					spots.append([i, rc - i, "r"])
 					if not marker:
-						marker=True
-	#If no 'corners' (2-neighbors), then choose from ones
-	if len(spots)==0:
+						marker = True
+	# If no 'corners' (2-neighbors), then choose from ones
+	if len(spots) == 0:
 		for r in range(b):
 			for c in range(b):
-				if board[r][c]==0 and neighbors(board,r,c)==1:
-					if checkdir(board,r,c,"u"):
-						spots.append([r,c,"u"])
-					if checkdir(board,r,c,"d"):
-						spots.append([r,c,"d"])
-					if checkdir(board,r,c,"l"):
-						spots.append([r,c,"l"])
-					if checkdir(board,r,c,"r"):
-						spots.append([r,c,"r"])
+				if board[r][c] == 0 and neighbors(board, r, c) == 1:
+					if checkdir(board, r, c, "u"):
+						spots.append([r, c, "u"])
+					if checkdir(board, r, c, "d"):
+						spots.append([r, c, "d"])
+					if checkdir(board, r, c, "l"):
+						spots.append([r, c, "l"])
+					if checkdir(board, r, c, "r"):
+						spots.append([r, c, "r"])
 
 	return spots
 
-#returns the number of tiled entries directly adjacent to the square at r,c in board
-def neighbors(board,r,c):
-	b=len(board)
-	count=0
-	if 0<=r and r<b and 0<=c+1 and c+1<b:
-		if board[r][c+1]!=0:
-			count=count+1
-	if 0<=r and r<b and 0<=c-1 and c-1<b:
-		if board[r][c-1]!=0:
-			count=count+1
-	if 0<=r+1 and r+1<b and 0<=c and c<b:
-		if board[r+1][c]!=0:
-			count=count+1
-	if 0<=r-1 and r-1<b and 0<=c and c<b:
-		if board[r-1][c]!=0:
-			count=count+1
+
+# returns the number of tiled entries directly adjacent to the square
+# at r, c in board
+def neighbors(board, r, c):
+	b = len(board)
+	count = 0
+	if 0 <= r and r < b and 0 <= c + 1 and c + 1 < b:
+		if board[r][c + 1] != 0:
+			count = count + 1
+	if 0 <= r and r < b and 0 <= c - 1 and c - 1 < b:
+		if board[r][c - 1] != 0:
+			count = count + 1
+	if 0 <= r + 1 and r + 1 < b and 0 <= c and c < b:
+		if board[r + 1][c] != 0:
+			count = count + 1
+	if 0 <= r - 1 and r - 1 < b and 0 <= c and c < b:
+		if board[r - 1][c] != 0:
+			count = count + 1
 	return count
 
-#fills in all tiles that follow deterministcally (3 neighbors on a square)
+
+# fills in all tiles that follow deterministcally (3 neighbors on a square)
 def fillBoard(board):
-	b=len(board)
-	#print self.b
-	#print self.board
-	runitback=True #this fills over and over again until there are no more fillable spots
+	b = len(board)
+	runitback = True  # this fills until there are no more fillable spots
 	while runitback:
-		runitback=False
+		runitback = False
 		for r in range(b):
 			for c in range(b):
-				#print str(r)+" "+str(c)
-				if board[r][c]==0 and neighbors(board,r,c)==3:
-					if checkdir(board,r,c,"u"):
-						fill(board,r,c,"u")
-						runitback=True
-					if checkdir(board,r,c,"d"):
-						fill(board,r,c,"d")
-						runitback=True
-					if checkdir(board,r,c,"l"):
-						fill(board,r,c,"l")
-						runitback=True
-					if checkdir(board,r,c,"r"):
-						fill(board,r,c,"r")
-						runitback=True
+				if board[r][c] == 0 and neighbors(board, r, c) == 3:
+					if checkdir(board, r, c, "u"):
+						fill(board, r, c, "u")
+						runitback = True
+					if checkdir(board, r, c, "d"):
+						fill(board, r, c, "d")
+						runitback = True
+					if checkdir(board, r, c, "l"):
+						fill(board, r, c, "l")
+						runitback = True
+					if checkdir(board, r, c, "r"):
+						fill(board, r, c, "r")
+						runitback = True
 
-#checks if the board has been fully tiled (returns True if NOT fully tiled)
+
+# checks if the board has been fully tiled (returns True if NOT fully tiled)
 def notfull(board):
-	b=len(board)
+	b = len(board)
 	for r in range(b):
 		for c in range(b):
-			if board[r][c]==0:
+			if board[r][c] == 0:
 				return True
 	return False
 
-#This is the main section of the program. It constructs the board, while it's not full
-#or stuck it iterates through finding all options on the diagonal and choosing randomly 
-#between them. Finally it prints it out in progress (optional) and prints it out at the end
+
+# This is the main section of the program. It constructs the board, while
+# it's not full or stuck it iterates through finding all options on the
+# diagonal and choosing randomly between them. Finally it prints it out in
+# progress (optional) and prints it out at the end
 #
-#b=dimension of square board to be randomly tiled with 1x2 tiles
-#displaytype="text" for text output, ="temp" for a temporary picture, and ="filename" (no extension) for saved picture
-def tile (b, displaytype):
-	board=initboard(b)
-	fill(board,0,0,"r")
-	n=0
+# b=dimension of square board to be randomly tiled with 1x2 tiles
+# displaytype="text" for text output, ="temp" for a temporary picture, and
+# ="filename" (no extension) for saved picture
+def tile(b, displaytype):
+	board = initboard(b)
+	fill(board, 0, 0, "r")
+	n = 0
 	while check(board) and notfull(board):
-		n=n+1
+		n = n + 1
 		fillBoard(board)
-		l=findSpots(board)
-		if len(l)==0:
+		l = findSpots(board)
+		if len(l) == 0:
 			print "NO POSSIBLE TILING OPTIONS?!?!"
 			displaytiling.disp(board)
 			continue
 		else:
-			r=random.randint(0,len(l)-1)
-			m=l[r]
-			fill(board,m[0],m[1],m[2])
+			r = random.randint(0, len(l) - 1)
+			m = l[r]
+			fill(board, m[0], m[1], m[2])
 	displaytiling.disp(board, displaytype)
-
-
-
